c7562d61cea927ee629f7614ca81fb6e8a07ea90
jfrank-sit <jfrank@summitinvest.tech>
2018-04-12 23:03:36 -0600

    adding some intellisense

--- SIT.ExcelAnalytics/xlfunctions.fs
+++ SIT.ExcelAnalytics/xlfunctions.fs
@@ -1,14 +1,15 @@
 ﻿namespace SIT.Analytics
 open System
 open ExcelDna.Integration
+open System.ComponentModel
     
 type xlStatisticFunctions =   
     
     [<ExcelFunction>]
     static member SayHi(toWho:string) = sprintf "Hi %s!" toWho
 
-    [<ExcelFunction>]
-    static member GeoMeanReturn(retData) =      
+    [<ExcelFunction(Name="GeoMeanReturn", Description="Geometric average of a return series", Category="Analytics", IsThreadSafe=true, IsVolatile=true)>]
+    static member GeoMeanReturn([<ExcelArgument(Name="ReturnSeries", Description="Return series")>] retData) =      
        let retStats = ReturnStatistics(retData)
        retStats.GeoMeanReturn
         
