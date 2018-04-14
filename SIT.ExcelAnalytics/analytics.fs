16f476e5ea802d2f3bd413b23a5af7acdb11c56d
jfrank-sit <jfrank@summitinvest.tech>
2018-04-13 21:29:12 -0600

    experimenting with different error catching

--- SIT.ExcelAnalytics/analytics.fs
+++ SIT.ExcelAnalytics/analytics.fs
@@ -1,23 +1,20 @@
 ﻿namespace SIT.Analytics
 open System
 
+
+type Statistic =
+    | StatVal
+    | Fail of string
 type ReturnStatistics(retData) =
     let totalReturn data = Array.fold (fun retAcc periodRet -> retAcc * (1. + periodRet) ) 1. data - 1.
     let periodReturn data n = (1. + (totalReturn data)) ** (n / float data.Length) - 1.
     
     //will handle errors more robustly in future
-    let ifErrNone func =
-        try
-            Some func
-        with 
-        | _ -> None
+    let errorCheck func = try Some func with | _ -> None 
 
-    member x.TotalReturn = 
-        ifErrNone (totalReturn retData)        
+    member x.TotalReturn =  totalReturn retData |> errorCheck
 
-    member x.AnnualReturn nPerYear = 
-        ifErrNone (periodReturn retData nPerYear)        
+    member x.AnnualReturn nPerYear = periodReturn retData nPerYear |> errorCheck     
 
-    member x.GeoAvgReturn =
-        ifErrNone (periodReturn retData 1.)
+    member x.GeoAvgReturn = periodReturn retData 1. |> errorCheck
         
